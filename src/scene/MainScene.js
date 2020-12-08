c57b14db35273afc29ac76664a2e685f4d2a9cdc
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2020-12-08 21:02:33 +0900

    update

--- src/scene/MainScene.js
+++ src/scene/MainScene.js
@@ -1,9 +1,11 @@
 import phina from 'phina.js'
 import Matter from 'matter-js'
-import { SCREEN, COLOR } from '../constants'
+import { SCREEN, COLOR, FONT, BASE_COLOR, MIXED_COLORS } from '../constants'
 
 import { MatterLayer } from '../matter/'
 import { CircleShape, RectangleShape, TriangleShape } from '../display/'
+
+import { cmyExact, cmyToRgb } from '../utils'
 
 export default phina.define('mc.scene.MainScene', {
   superClass: phina.display.DisplayScene,
@@ -11,8 +13,21 @@ export default phina.define('mc.scene.MainScene', {
   /** MatterLayer */
   mtLayer: null,
 
+  /** cmy colors */
+  baseColors: null,
+  mixedColors: null,
+
+  level: null,
+  stage: null,
+  time: null,
+
+  answers: null,
+
   init(options) {
     this.superInit(options)
+
+    this.baseColors = Object.values(BASE_COLOR)
+    this.mixedColors = MIXED_COLORS
 
     this.backgroundColor = COLOR.BASE
 
@@ -25,67 +40,44 @@ export default phina.define('mc.scene.MainScene', {
 
     this._createWall()
 
-    const cmyList = [
-      { c: 1, m: 0, y: 0 },
-      { c: 0, m: 1, y: 0 },
-      { c: 0, m: 0, y: 0 },
-      { c: 0, m: 0, y: 1 },
-      { c: 1, m: 0, y: 0 },
-    ]
+    this.topBar = phina.display
+      .RectangleShape({
+        fill: '#fff',
+        stroke: 'transparent',
+        width: SCREEN.W,
+        height: SCREEN.H / 8,
+        x: SCREEN.W / 2,
+        y: SCREEN.H / 16,
+      })
+      .addChildTo(this)
 
-    RectangleShape({
-      width: 100,
-      height: 100,
-      x: 250,
-      y: 250,
-      cmyList,
-    }).addChildTo(this.mtLayer)
+    const labelOptions = {
+      fill: '#888',
+      fontSize: 28,
+      fontFamily: FONT.DEFAULT,
+      fontWeight: '300',
+      align: 'left',
+      x: 10,
+    }
 
-    CircleShape({
-      radius: 56,
-      x: 500,
-      y: 500,
-      cmyList,
-    }).addChildTo(this.mtLayer)
+    this.stageLabel = phina.display
+      .Label({
+        ...labelOptions,
+        text: `stage: 1`,
+        y: 30,
+      })
+      .addChildTo(this)
 
-    TriangleShape({
-      radius: 87,
-      x: 250,
-      y: 500,
-      cmyList,
-    }).addChildTo(this.mtLayer)
+    this.timeLabel = phina.display
+      .Label({
+        ...labelOptions,
+        text: `time:\n  00:01`,
+        y: 80,
+      })
+      .addChildTo(this)
 
     this.mtLayer.on('collisionStart', (e) => {
-      const { bodyA, bodyB } = e.pairs[0]
-      const mcObjA = bodyA.plugin.mcObject
-      const mcObjB = bodyB.plugin.mcObject
-
-      // 壁と衝突したとき
-      if (bodyA.plugin.mcLabel === 'wall') {
-        if (bodyB.plugin.isDrag || mcObjB.cmyList.length === 1 || bodyB.speed < 100) return
-
-        mcObjB.cmyList.forEach((cmy) => {
-          this._spawn(bodyB.plugin.mcLabel, bodyB.position, [cmy])
-        })
-
-        mcObjB.remove()
-      }
-      // 同じ種類のオブジェクト同士
-      else if (bodyA.plugin.mcLabel === bodyB.plugin.mcLabel) {
-        // どちらもドラッグ中でなければreturn
-        if (!bodyA.plugin.isDrag && !bodyB.plugin.isDrag) return
-        if (mcObjA.cmyList.length + mcObjB.cmyList.length > 5) return
-
-        if (bodyA.speed < bodyB.speed) {
-          mcObjB.cmyList.push(...mcObjA.cmyList)
-          mcObjB.applyCmyList()
-          mcObjA.remove()
-        } else {
-          mcObjA.cmyList.push(...mcObjB.cmyList)
-          mcObjA.applyCmyList()
-          mcObjB.remove()
-        }
-      }
+      this._onCollision(e)
     })
 
     this.mtLayer.on('startdrag', (e) => {
@@ -94,40 +86,162 @@ export default phina.define('mc.scene.MainScene', {
     this.mtLayer.on('enddrag', (e) => {
       e.body.plugin.isDrag = false
     })
+
+    this.time = 0
+    this.level = 2
+    this.stage = 1
+
+    this._createStage()
   },
 
-  _spawn(type, position, cmyList) {
-    const area = 10000
+  update(app) {
+    this.time += app.ticker.deltaTime
+
+    const sec = Math.floor(this.time / 1000)
+    const min = Math.floor(sec / 60)
+
+    const strSec = (sec % 60).padding(2, '0')
+    const strMin = min.padding(2, '0')
+
+    this.timeLabel.text = `time:\n  ${strMin}:${strSec}`
+  },
+
+  _spawn(type, cmy, position) {
+    const options = {
+      cmyList: [cmy],
+    }
 
     let shape
     switch (type) {
       case 'rectangle':
-        shape = RectangleShape({
-          width: Math.sqrt(area),
-          height: Math.sqrt(area),
-          isInvalid: true,
-          cmyList,
-        })
+        shape = RectangleShape(options)
         break
       case 'circle':
-        shape = CircleShape({
-          radius: Math.floor(Math.sqrt(area / Math.PI)),
-          isInvalid: true,
-          cmyList,
-        })
+        shape = CircleShape(options)
         break
       case 'triangle':
-        shape = TriangleShape({
-          radius: 87,
-          isInvalid: true,
-          cmyList,
-        })
+        shape = TriangleShape(options)
+        break
     }
 
-    shape
-      .addChildTo(this.mtLayer)
-      .mtSetPosition(position)
-      .mtSetAngularVelocity(Math.PI / 6)
+    position = position || { x: SCREEN.W / 2, y: SCREEN.H / 2 }
+
+    shape.addChildTo(this.mtLayer).mtSetPosition(position)
+    shape.mtSetAngularVelocity(Math.randfloat(-0.5, 0.5))
+  },
+
+  _onCollision(e) {
+    const { bodyA, bodyB } = e.pairs[0]
+    const mcObjA = bodyA.plugin.mcObject
+    const mcObjB = bodyB.plugin.mcObject
+
+    // 壁と衝突したとき
+    if (bodyA.plugin.mcLabel === 'wall') {
+      if (bodyB.plugin.isDrag || mcObjB.cmyList.length === 1 || bodyB.speed < 30) return
+
+      mcObjB.cmyList.forEach((cmy) => {
+        this._spawn(bodyB.plugin.mcLabel, cmy, bodyB.position)
+      })
+      mcObjB.remove()
+
+      this._checkClear()
+    }
+    // 同じ種類のオブジェクト同士
+    else if (bodyA.plugin.mcLabel === bodyB.plugin.mcLabel) {
+      // どちらもドラッグ中でなければreturn
+      if (!bodyA.plugin.isDrag && !bodyB.plugin.isDrag) return
+      if (mcObjA.cmyList.length + mcObjB.cmyList.length > 6) return
+
+      if (bodyB.plugin.isDrag) {
+        mcObjB.cmyList.push(...mcObjA.cmyList)
+        mcObjB.applyCmyList()
+        mcObjA.remove()
+      } else {
+        mcObjA.cmyList.push(...mcObjB.cmyList)
+        mcObjA.applyCmyList()
+        mcObjB.remove()
+      }
+
+      this._checkClear()
+    }
+  },
+
+  _checkClear() {
+    this.answers.forEach((ans) => {
+      ans.isCorrect = false
+      ans.starShape.fill = '#fff'
+    })
+
+    objLoop: for (const obj of this.mtLayer.children) {
+      for (const ans of this.answers) {
+        if (!ans.isCorrect && cmyExact(obj.cmyList, ans.cmyList)) {
+          ans.isCorrect = true
+          ans.starShape.fill = ans.starShape.stroke
+          continue objLoop
+        }
+      }
+    }
+
+    if (this.answers.every((v) => v.isCorrect)) {
+      const children = this.mtLayer.children
+      const len = children.length
+
+      for (let i = 0; i < len; ++i) {
+        children[0].remove()
+      }
+      this.topBar.children.clear()
+
+      this.stage++
+      this._createStage()
+    }
+  },
+
+  _createStage() {
+    const answers = []
+
+    const shapeTypeList = ['rectangle', 'circle', 'triangle']
+
+    const numOfQue = this.level + 1
+    let prevShapeIndex = Math.randint(0, 2)
+
+    for (let i = 0; i < numOfQue; i++) {
+      const cmyList = this.mixedColors.pickup()
+
+      const shapeIndex = (Math.randint(1, 2) + prevShapeIndex) % 3
+      const type = shapeTypeList[shapeIndex]
+
+      cmyList.forEach((cmy) => {
+        this._spawn(type, cmy)
+      })
+
+      // ダミーを生成
+      this._spawn(type, this.baseColors.pickup())
+      this._spawn(type, this.baseColors.pickup())
+
+      prevShapeIndex = shapeIndex
+
+      const offsetX = (numOfQue - 1) * 50
+      const starShape = phina.display
+        .StarShape({
+          padding: 20,
+          radius: 38,
+          x: i * 100 - offsetX,
+          fill: '#fff',
+          stroke: cmyToRgb(cmyList),
+          strokeWidth: 20,
+          sideIndent: 0.5,
+        })
+        .addChildTo(this.topBar)
+
+      answers.push({
+        starShape,
+        cmyList,
+        isCorrect: false,
+      })
+    }
+
+    this.answers = answers
+    this.stageLabel.text = `stage: ${this.stage}`
   },
 
   _createWall() {
@@ -139,7 +253,7 @@ export default phina.define('mc.scene.MainScene', {
 
     const walls = [
       // top
-      Matter.Bodies.rectangle(SCREEN.W / 2, -size / 2, SCREEN.W + size * 2, size, options),
+      Matter.Bodies.rectangle(SCREEN.W / 2, -size / 2 + SCREEN.H / 8, SCREEN.W + size * 2, size, options),
       // bottom
       Matter.Bodies.rectangle(SCREEN.W / 2, SCREEN.H + size / 2, SCREEN.W + size * 2, size, options),
       // left
