4fce0a78c2e31d1c705b1198e4e62c259cdc163d
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2020-12-08 21:01:51 +0900

    add: utils.jsを追加

---
+++ src/utils.js
@@ -0,0 +1,50 @@
+/**
+ * cmy同士の足し算の結果を返します
+ * @param {cmy[]} cmyList
+ * @returns {cmy} cmy
+ */
+export const cmySum = (cmyList) => {
+  return cmyList.reduce(
+    (a, c) => {
+      return {
+        c: a.c + c.c,
+        m: a.m + c.m,
+        y: a.y + c.y,
+      }
+    },
+    { c: 0, m: 0, y: 0 }
+  )
+}
+
+/**
+ * cmyList同士を比較して同じ色か確認します
+ * @param {cmy[]} cmyListA
+ * @param {cmy[]} cmyListB
+ * @returns {boolean} exact
+ */
+export const cmyExact = (cmyListA, cmyListB) => {
+  const cmyA = cmySum(cmyListA)
+  const cmyB = cmySum(cmyListB)
+
+  return cmyListA.length === cmyListB.length && cmyA.c === cmyB.c && cmyA.m === cmyB.m && cmyA.y === cmyB.y
+}
+
+/**
+ * cmyListからrgbの形式に変換します
+ * @param {cmy[]} cmyList
+ * @returns {string} rgb
+ */
+export const cmyToRgb = (cmyList) => {
+  const cmy = cmySum(cmyList)
+
+  const len = cmyList.length
+  const c = (cmy.c / len) * 0.7
+  const m = (cmy.m / len) * 0.7
+  const y = (cmy.y / len) * 0.7
+
+  const r = Math.floor((1 - c) * 255)
+  const g = Math.floor((1 - m) * 255)
+  const b = Math.floor((1 - y) * 255)
+
+  return `rgb(${r}, ${g}, ${b})`
+}
